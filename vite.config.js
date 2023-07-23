091166f27eecfa6c6c925839347f39c00a5c001b
Rafael Moreira <rafaeltimoreirass@gmail.com>
2023-07-23 05:09:20 -0300

    Vite pdf

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/educacao-infantil/",
+  build: {
+    assetsInclude: ['**/*.pdf'], // Incluir arquivos PDF na build
+  },
 })
