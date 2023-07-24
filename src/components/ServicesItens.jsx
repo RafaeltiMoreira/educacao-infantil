7fababc22f5caa9d6b42040116215d26bb84d7b4
Rafael Moreira <rafaeltimoreirass@gmail.com>
2023-07-24 00:28:42 -0300

    Update PDF

--- src/components/ServicesItens.jsx
+++ src/components/ServicesItens.jsx
@@ -1,11 +1,8 @@
-import React from 'react';
-import { pdfjs } from 'react-pdf';
-
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+import React, { useState } from 'react';
 
 export default function ServicesItens({ title, icon, description, embedPdf }) {
+  
+
   return (
     <div className='bg-slate-300 hover:bg-slate-200 dark:bg-slate-800 dark:hover:bg-slate-700 hover:-translate-y-2 transform transition p-5 rounded-md'>
       <div className='text-gray-800 dark:text-gray-300 w-6 h-6 mb-3'>
@@ -14,10 +11,15 @@ export default function ServicesItens({ title, icon, description, embedPdf }) {
       <h1 className='font-semibold text-xl text-gray-600 dark:text-gray-200 mb-1'>{title}</h1>
       <p className='text-md text-gray-600 dark:text-gray-300'>{description}</p>
       <div className='w-full mt-4'>
-        <div className='w-full'>
-          {embedPdf}
+        <div className='mt-8 w-full'>
+          <object data={embedPdf} type='application/pdf' width='100%' height='390px'>
+            <p>Seu navegador não suporta a visualização de PDFs. Você pode baixar o arquivo 
+              <a href={embedPdf}>aqui</a>.
+            </p>
+          </object>
         </div>
       </div>
+
     </div>
   );
 }
--- src/data/services.jsx
+++ src/data/services.jsx
@@ -9,12 +9,18 @@ export default [
         title: 'Cartilha Indígenas',
         icon: <IoBookOutline className='w-full h-full' />,
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit!',
-        embedPdf: <iframe className='md:w11/12 w-full object-cover rounded-md' src={indigenas} type="application/pdf" height={390} />,
+        embedPdf: indigenas,
     },
     {
         title: 'Afroteca',
         icon: <IoBook className='w-full h-full' />,
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit!',
-        embedPdf: <iframe className='md:w11/12 w-full object-cover rounded-md' src={afroteca} type="application/pdf" height={390} />,
+        embedPdf: afroteca,
     }
 ]
+
+/*
+<iframe src={indigenas} className='md:w11/12 w-full object-cover rounded-md'  type="application/pdf" height={390} />
+
+<iframe src={afroteca} className='md:w11/12 w-full object-cover rounded-md'  type="application/pdf" height={390} />,
+*/
